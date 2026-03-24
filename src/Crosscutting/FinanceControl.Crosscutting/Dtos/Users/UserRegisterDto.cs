7c67195db4b588dee49b2fa7e13e3d68842c8f65
Julio Mattos <julio.mattos999@gmail.com>
2026-03-23 23:12:50 -0300

    iniciado desenvolvimento da tela de cadastro

---
+++ src/Crosscutting/FinanceControl.Crosscutting/Dtos/Users/UserRegisterDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FinanceControl.Domain.Entities;
+
+public class RegisterUserDto
+{
+    [Required(ErrorMessage = "Nome de usuário é obrigatório")]
+    [MinLength(3, ErrorMessage = "Mínimo 3 caracteres")]
+    public string Username { get; set; } = "";
+
+    [Required(ErrorMessage = "E-mail é obrigatório")]
+    [EmailAddress(ErrorMessage = "E-mail inválido")]
+    public string Email { get; set; } = "";
+
+    [Required(ErrorMessage = "Senha é obrigatória")]
+    [MinLength(8, ErrorMessage = "Mínimo 8 caracteres")]
+    public string Password { get; set; } = "";
+
+    public string PhtoBase64 { get; set; } = "";
+}
