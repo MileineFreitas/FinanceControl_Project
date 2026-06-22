e6bffdf34af588e309b6ea44365684054e826b35
Mileine Freitas <mileine.freitas29@gmail.com>
2026-06-21 23:11:49 -0300

    criado tela de privacidade, configuração, e ajustes de front diversos

--- src/Presentation/FinanceControl.Web.App/Controllers/Account/AccountController.cs
+++ src/Presentation/FinanceControl.Web.App/Controllers/Account/AccountController.cs
@@ -8,6 +8,14 @@ public class AccountController : Controller
     [HttpGet("configuracao")]
     public IActionResult Configuracao() => View("Configuration");
 
+    [HttpPost("configuracao")]
+    [ValidateAntiForgeryToken]
+    public IActionResult Configuracao(IFormCollection form)
+    {
+        TempData["ConfigSucesso"] = "Configurações salvas com sucesso.";
+        return RedirectToAction(nameof(Configuracao));
+    }
+
     [HttpGet("privacidade")]
     public IActionResult Privacidade() => View("Privacy");
 }
